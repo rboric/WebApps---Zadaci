ff272bfe3934de352f1573723dd3c0ceace4372c
Rene Borić <rboric1901@gmail.com>
2022-03-24 17:04:43 +0100

    zadaca

--- wa-001/src/App.vue
+++ wa-001/src/App.vue
@@ -5,4 +5,29 @@
   </div>
 </template>
 
+<script>
+export default {
+  methods: {
+    getData() {
+      axios
+        .get("https://api.github.com/repos/vuejs/vue/commits")
+        .then((response) => {
+          response.data.forEach((commits) => {
+            this.info.push({
+              SHA: commits.sha,
+              IME: commits.commit.author.name,
+              EMAIL: commits.commit.author.email,
+              PORUKA: commits.commit.message,
+              DATUM: commits.commit.committer.date,
+            });
+          });
+        });
+    },
+  },
+  mounted() {
+    this.getData();
+  },
+};
+</script>
+
 <style lang="scss"></style>
